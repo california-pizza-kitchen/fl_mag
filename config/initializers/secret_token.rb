70cb765ad377f2cd7b2c627635f8271b2d4cf196
Kyle Shike <kyleshike@gmail.com>
2013-11-18 17:46:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogAggregator::Application.config.secret_key_base = '0fad10c75257be37b80cd40c9840a9c8b215dbd0deebc3449daff9246602c28b02fd4e467c7d28378484aec59bf87a21c4bed41acb51a0f74d11fc234961b027'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogAggregator::Application.config.session_store :cookie_store, key: '_blog_aggregator_session'
