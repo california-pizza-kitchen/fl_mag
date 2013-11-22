304c48c880c2f37365711bd4b8011b1a94537b4b
Kyle Shike <kyleshike@gmail.com>
2013-11-22 11:19:41 -0500

    remove superfluous method

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,12 +6,6 @@ class EntriesController < ApplicationController
     @blogger = Blogger.find_by(:slug => params[:blogger_slug])
   end
 
-  def update
-    @entry = Entry.find_by(:slug => params[:slug])
-    @entry.update(:added? => true)
-    redirect_to '/users/1'
-  end
-
   def publish
     @entry = Entry.find_by(:slug => params[:slug])
     @entry.update(:added? => true)
