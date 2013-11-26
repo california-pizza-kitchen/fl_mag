d5200e0792c8cdc6cad4869f296f45f44a4d5bc0
Kyle Shike <kyleshike@gmail.com>
2013-11-26 13:39:22 -0500

    work on javascript business

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,12 +3,9 @@ $( document ).ready(function() {
 
 
     $("#about").click(function(){
-      $(".jumbotron").fadeOut(500,function(){
-        $("#jumbo-title").text("curated from current students and alumni.");
-        $("#jumbo-body").text("“when you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.” ― Why The Lucky Stiff");
+      $(".full-content").fadeOut(500, function(){
+        $(".about-content").fadeIn(1000);
       });
-
-      $(".jumbotron").fadeIn(1000);
     });
 
 
--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -16,4 +16,3 @@ class EmailWorker
   end
 
 end
-
