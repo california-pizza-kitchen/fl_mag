99795dbed478b433bef38cdaf016bc3160acbf95
Patrick Janson <pjlacrosse@gmail.com>
2013-12-04 17:18:24 -0500

    mailgun in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,3 +92,50 @@ end
 
 after "deploy:setup",           "db:setup"   unless fetch(:skip_db_setup, false)
 after "deploy:finalize_update", "db:symlink"
+
+namespace :settings do
+  desc <<-DESC
+    Creates the database.yml configuration file in shared path.
+
+    By default, this task uses a template unless a template 
+    called database.yml.erb is found either is :template_dir 
+    or /config/deploy folders. The default template matches 
+    the template for config/database.yml file shipped with Rails.
+
+    When this recipe is loaded, db:setup is automatically configured 
+    to be invoked after deploy:setup. You can skip this task setting 
+    the variable :skip_db_setup to true. This is especially useful 
+    if you are using this recipe in combination with 
+    capistrano-ext/multistaging to avoid multiple db:setup calls 
+    when running deploy:setup for all stages one by one.
+  DESC
+  task :setup, :except => { :no_release => true } do
+
+    default_template = <<-EOF
+      MAILGUN_USERNAME: 
+      MAILGUN_PASSWORD: 
+      MAILGUN_DOMAIN: 
+      TWITTER_PUBLIC: 
+      TWITTER_PRIVATE: 
+    EOF
+
+    location = fetch(:template_dir, "config/") + '/application.yml'
+    template = File.file?(location) ? File.read(location) : default_template
+
+    config = ERB.new(template)
+
+    run "mkdir -p #{shared_path}/config"
+    put config.result(binding), "#{shared_path}/config/application.yml"
+  end
+
+  desc <<-DESC
+    [internal] Updates the symlink for database.yml file to the just deployed release.
+  DESC
+  task :symlink, :except => { :no_release => true } do
+    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+  end
+
+end
+
+after "deploy:setup",           "settings:setup"   unless fetch(:skip_settings_setup, false)
+after "deploy:finalize_update", "settings:symlink"
