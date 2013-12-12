c538c2a135cbf8eb56c69515839d9999eb9bf8c9
Patrick Janson <pjlacrosse@gmail.com>
2013-12-12 13:23:24 -0500

    clean up

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,17 +1,9 @@
 $( document ).ready(function() {
 
-
-
     $("#about").click(function(){
-      // if (".")
       $(".full-content").fadeOut(500, function(){
         $(".about-content").fadeIn(1000);
       });
     }); 
 
-    // $("#magazine").click(function(){
-    //   $(".about-content").fadeOut(300, function(){
-    //   $(".full-content").fadeIn(400);
-    //   });
-    // });
 });
