c0f1eddec8c835f37ae7e06941a55b178b4ba6d0
Chris Callahan <callahan.c@gmail.com>
2014-08-05 16:18:45 -0400

    namespace api routes to /api/v0/

---
+++ app/controllers/api/v0/tags_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V0
+    class TagsController < ApplicationController
+      def show
+        @tag = Tag.find_by(word: params[:word])
+        render json: @tag.as_json_verbose
+      end
+
+      def index
+        @tags = Tag.all
+        render json: @tags.map(&:as_json_concise)
+      end
+    end
+  end
+end
--- app/models/school_session.rb
+++ app/models/school_session.rb
@@ -9,13 +9,39 @@ class SchoolSession < ActiveRecord::Base
     self.slug = self.name.downcase.gsub(/[\W,\s]/,'-')
   end
 
-
   def bloggers_count
     self.bloggers.count
   end
 
   def entries_count
     self.entries.count
+  end
+
+  def as_json
+    {
+      "name" => self.name,
+      "slug" => self.slug,
+      "_self" => "#{ENV['API_ROOT']}/school_sessions/#{self.slug}",
+      "bloggers_count" => self.bloggers_count,
+      "bloggers" => self.bloggers.collect{|blogger|
+          {
+            "name" => blogger.name,
+            "number_of_entries" => blogger.entries.length,
+            "_self" => "#{ENV['API_ROOT']}/bloggers/#{blogger.slug}"
+          }
+        },
+      "entries_count" => self.entries_count,
+      "entries" => self.entries.collect do |entry|
+        {
+          "title" => entry.title,
+          "_self" => "#{ENV['API_ROOT']}/bloggers/#{entry.feed.blogger.slug}/entries/#{entry.slug}",
+          "blogger" =>  {
+                          "name" => entry.feed.blogger.name,
+                          "_self" => "#{ENV['API_ROOT']}/bloggers/#{entry.feed.blogger.slug}"
+                        }
+        }
+      end
+    }
   end
 
 end
