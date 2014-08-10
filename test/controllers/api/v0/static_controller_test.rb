d598a4b81b8dc01d859d206ba9fb0a1b5e4f79f3
pcrglennon <pcrglennon@gmail.com>
2014-08-10 14:15:20 -0400

    Remove obsolete 'test' directory

--- test/controllers/api/v0/static_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class API::V0::StaticControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/api/v0/static_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class API::V0::StaticHelperTest < ActionView::TestCase
-end
